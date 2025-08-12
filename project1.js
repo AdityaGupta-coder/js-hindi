897129e5c33c72887ea1e81e73a14d95947377c5
AdityaGupta-coder <adityakgupta0555@gmail.com>
2025-08-12 21:50:22 +0000

    Save Changes

--- project1.js
+++ project1.js
@@ -2,12 +2,18 @@
     const buttons = document.querySelectorAll('.button');
     const body = document.querySelector("body");
 
-    buttons.forEach(function (btn) {
-        btn.addEventListener('click', function(e) {
+    buttons.forEach(function (button) {
+        button.addEventListener('click', function(e) {
             if (e.target.id === 'grey') {
                 body.style.backgroundColor = 'green';
-            } else if (e.target.id === 'blue') {
+            } if(e.target.id === 'blue') {
                 body.style.backgroundColor = 'orange';
+            }
+            if(e.target.id === 'white') {
+                body.style.backgroundColor = 'black';
+            }
+            if(e.target.id === 'yellow') {
+                body.style.backgroundColor = 'pink';
             }
         });
     });
