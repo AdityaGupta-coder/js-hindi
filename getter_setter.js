e119473b70daa4d08b134333848c01459ea11142
AdityaGupta-coder <adityakgupta0555@gmail.com>
2025-08-29 21:15:14 +0000

    new topic

---
+++ getter_setter.js
@@ -0,0 +1,24 @@
+// ------------------------------------ Getter & Setter  ------------------------------------
+// use of getter & setter this protect to show directly actual data or info to user 
+class user {
+//     constructor( username , userId , Password){
+//         this.username = username,
+//         this.userId = userId,
+//         this.Password= Password
+     }
+// // both getter & setter has same function name as declare in constructor
+//     get Password(){
+//         return this._Password.toUpperCase()
+//     }
+//     set Password( pass){
+       
+//     //this.Password = pass // when you directly set value then contiuesly set by both setter & constructor it cause error
+//         this._Password = pass ;
+//     }
+// }
+// const output = new user( "adityakumargupta" , "@aditya0555","xyz");
+// console.log(output.Password);
+
+
+// ---------------------------- Lexical Scope & closure ------------------------------
+
